3cf67b3136b1b7bdb7226bb4847d89c6b15b9910
Fredrik GB <43887343+gbfredrik@users.noreply.github.com>
2020-02-11 22:58:00 +0100

    Added gitignore + avg func
    Created shell for DailyAverage function and added gitignore file

---
+++ DataParsing/DailyAverage.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = DailyAverage(inputArg1,inputArg2)
+%DAILYAVERAGE Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
