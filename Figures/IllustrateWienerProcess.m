fcadf2d5ed4afaa134217f4421be0dba8b081c6a
Fredrik GB <43887343+gbfredrik@users.noreply.github.com>
2020-03-29 16:25:23 +0200

    Created two placeholders for BM and DAT

---
+++ Figures/IllustrateWienerProcess.m
@@ -0,0 +1,10 @@
+%% Generate Wiener process graph
+% Used to plot a simple Wiener process / Brownian motion for illustration
+%test = bm(1, 1);
+%n = normrnd(0, 1, 1000);
+%for k = 1 : 1000
+%    noise(1,k) = sum(n(1:k));
+%end
+%series = 0.025*(1:1000)/252 + 0.4/sqrt(252) * noise;
+%figure(100);
+%plot(series)
