fda3d3a2537b7610f9e9077544071d2842f7e15d
Fredrik GB <43887343+gbfredrik@users.noreply.github.com>
2020-05-05 01:02:16 +0200

    Updated code, still needs work

--- Figures/IllustrateWienerProcess.m
+++ Figures/IllustrateWienerProcess.m
@@ -1,10 +1,13 @@
 %% Generate Wiener process graph
-% Used to plot a simple Wiener process / Brownian motion for illustration
-%test = bm(1, 1);
-%n = normrnd(0, 1, 1000);
-%for k = 1 : 1000
-%    noise(1,k) = sum(n(1:k));
-%end
-%series = 0.025*(1:1000)/252 + 0.4/sqrt(252) * noise;
-%figure(100);
-%plot(series)
+%BPATH2  Brownian path simulation: vectorized
+%Credit for code goes to: https://sites.me.ucsb.edu/~moehlis/APC591/tutorials/tutorial7/node2.html
+
+rng('default')              % set the state of randn
+T = 2; N = 365*2; dt = T/N;
+
+dW = sqrt(dt)*randn(1,N);   % increments
+W = cumsum(dW);             % cumulative sum
+
+plot([0:dt:T],[0,W],'r-')   % plot W against t
+xlabel('t','FontSize',16)
+ylabel('W(t)','FontSize',16,'Rotation',0)
