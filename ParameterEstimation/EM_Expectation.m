c0f06723a6538c97a73ceb873fd92259fd8b6ce1
Fredrik GB <43887343+gbfredrik@users.noreply.github.com>
2020-09-25 10:36:44 +0200

    Corrected log(f_GH). Implemented option to not use Kim's smoothing.

--- ParameterEstimation/EM_Expectation.m
+++ ParameterEstimation/EM_Expectation.m
@@ -1,4 +1,4 @@
-function [Pr1, Pr2, Pr1T, Pr2T] = EM_Expectation(T_deseason, p, theta_base, theta_shift, model)
+function [Pr1, Pr2, Pr1T, Pr2T] = EM_Expectation(T_deseason, p, theta_base, theta_shift, model, useKim)
 %EMEXPECTATION Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -17,6 +17,9 @@ for t = 2 : length(T_deseason) %
         mu_2 = theta_shift(1);
         sigma_2 = theta_shift(2);
         
+        p_1 = theta_base(3);
+        p_2 = theta_shift(3);
+        
         f1 = 1 / (sigma_1 * abs(T_deseason(t-1)) * sqrt(2*pi)) ... % TODO: abs() HERE?
             * exp(-1/2 * ((T_deseason(t) - (1 + kappa) * T_deseason(t-1)) ...
             / (sigma_1 * T_deseason(t-1)))^2);
@@ -31,6 +34,9 @@ for t = 2 : length(T_deseason) %
         mu_2 = theta_shift(1);
         sigma_2 = theta_shift(2);
         
+        p_1 = theta_base(4);
+        p_2 = theta_shift(3);
+        
         f1 = 1 / (sigma_1 * abs(T_deseason(t-1)) * sqrt(2*pi)) ...
             * exp(-1/2 * ((T_deseason(t) - ((1 - beta) * T_deseason(t-1) + mu_1)) ...
             / (sigma_1 * T_deseason(t-1)))^2);
@@ -39,41 +45,52 @@ for t = 2 : length(T_deseason) %
             / (sigma_2))^2);
     end
     
-    % Compute forecast probabilities:
-    Pr1_tminus1 = Pr1(t-1) * p(1, 1) + Pr2(t-1) * p(2, 1);
-    Pr2_tminus1 = Pr1(t-1) * p(1, 2) + Pr2(t-1) * p(2, 2);
-    
-    % Compute state membership probability for time t
-    Pr1(t) = Pr1_tminus1 * f1 ...
-        / (Pr1_tminus1 * f1 + Pr2_tminus1 * f2);
-    Pr2(t) = Pr2_tminus1 * f2 ...
-        / (Pr1_tminus1 * f1 + Pr2_tminus1 * f2);
-    
+    if (~useKim)
+        Pr1(t) = p_1 * f1 ...
+            / (p_1 * f1 + p_2 * f2);
+        Pr2(t) = p_2 * f2 ...
+            / (p_1 * f1 + p_2 * f2);
+    else
+        % Compute forecast probabilities:
+        Pr1_tminus1 = Pr1(t-1) * p(1, 1) + Pr2(t-1) * p(2, 1);
+        Pr2_tminus1 = Pr1(t-1) * p(1, 2) + Pr2(t-1) * p(2, 2);
+
+        % Compute state membership probability for time t
+        Pr1(t) = Pr1_tminus1 * f1 ...
+            / (Pr1_tminus1 * f1 + Pr2_tminus1 * f2);
+        Pr2(t) = Pr2_tminus1 * f2 ...
+            / (Pr1_tminus1 * f1 + Pr2_tminus1 * f2);
+    end
     %plot(t,f1,'.');
     %pause(0.001)
 end
 
-% Smooth state probabilities using Kim's smoothing algorithm
-Pr1T(length(T_deseason)) = Pr1(length(T_deseason));
-Pr2T(length(T_deseason)) = Pr2(length(T_deseason));
+if (~useKim)
+    Pr1T = Pr1;
+    Pr2T = Pr2;
+else
+    % Smooth state probabilities using Kim's smoothing algorithm
+    Pr1T(length(T_deseason)) = Pr1(length(T_deseason));
+    Pr2T(length(T_deseason)) = Pr2(length(T_deseason));
 
-for t = length(T_deseason)-1 : -1 : 1
-    % First compute forecast probabilities:
-    %Pr1T_tplus1 = Pr1T(t) * p(1, 1) + Pr2T(t) * p(2, 1);
-    %Pr2T_tplus1 = Pr1T(t) * p(1, 2) + Pr2T(t) * p(2, 2);
-    % ... and:
-    Pr1_tplus1 = Pr1(t) * p(1, 1) + Pr2(t) * p(2, 1);
-    Pr2_tplus1 = Pr1(t) * p(1, 2) + Pr2(t) * p(2, 2);
-    
-    % Smooth probabilities by filtering over the full filtration F_T
-    Pr1T(t) = (Pr1(t) * Pr1T(t+1) * p(1, 1)) / (Pr1_tplus1) ...
-        + (Pr1(t) * Pr2T(t+1) * p(1, 2)) / (Pr2_tplus1);
-    
-    Pr2T(t) = (Pr2(t) * Pr1T(t+1) * p(2, 1)) / (Pr1_tplus1) ...
-        + (Pr2(t) * Pr2T(t+1) * p(2, 2)) / (Pr2_tplus1);
-    
-    %fprintf("Pr1_tplus1: %.3f, Pr2_tplus1: %.3f. Pr1T(t): %.3f, Pr2T(t): %.3f.\n\n",...
-    %    Pr1_tplus1, Pr2_tplus1, Pr1T(t), Pr2T(t));
+    for t = length(T_deseason)-1 : -1 : 1
+        % First compute forecast probabilities:
+        %Pr1T_tplus1 = Pr1T(t) * p(1, 1) + Pr2T(t) * p(2, 1);
+        %Pr2T_tplus1 = Pr1T(t) * p(1, 2) + Pr2T(t) * p(2, 2);
+        % ... and:
+        Pr1_tplus1 = Pr1(t) * p(1, 1) + Pr2(t) * p(2, 1);
+        Pr2_tplus1 = Pr1(t) * p(1, 2) + Pr2(t) * p(2, 2);
+
+        % Smooth probabilities by filtering over the full filtration F_T
+        Pr1T(t) = (Pr1(t) * Pr1T(t+1) * p(1, 1)) / (Pr1_tplus1) ...
+            + (Pr1(t) * Pr2T(t+1) * p(1, 2)) / (Pr2_tplus1);
+
+        Pr2T(t) = (Pr2(t) * Pr1T(t+1) * p(2, 1)) / (Pr1_tplus1) ...
+            + (Pr2(t) * Pr2T(t+1) * p(2, 2)) / (Pr2_tplus1);
+
+        %fprintf("Pr1_tplus1: %.3f, Pr2_tplus1: %.3f. Pr1T(t): %.3f, Pr2T(t): %.3f.\n\n",...
+        %    Pr1_tplus1, Pr2_tplus1, Pr1T(t), Pr2T(t));
+    end
 end
 
 end
--- ParameterEstimation/EM_Maximization.m
+++ ParameterEstimation/EM_Maximization.m
@@ -1,4 +1,4 @@
-function [Theta, p, Q] = EM_Maximization(T_deseason, Pr1, Pr2, Pr1T, Pr2T, p, theta_base, theta_shift, model)
+function [Theta, p, Q] = EM_Maximization(T_deseason, Pr1, Pr2, Pr1T, Pr2T, p, theta_base, theta_shift, model, useKim)
 %EM_MAXIMIZATION Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -16,6 +16,7 @@ if (model == 1)
 
     sigma2_n1 = sqrt((sum(Pr2T(3:end)' .* (T_deseason(3:end) - (T_deseason(2:end-1) + theta_shift(1))).^2)) ...
         / (sum(Pr2T(3:end))));
+    
 elseif (model == 2)
     betaNumHelper = (sum(Pr1T(3:end)' .* T_deseason(2:end-1).^(-2) .* (T_deseason(3:end) - T_deseason(2:end-1)))) ...
         / (sum(Pr1T(3:end)' .* T_deseason(2:end-1).^(-2)));
@@ -29,8 +30,7 @@ elseif (model == 2)
         / (sum(Pr1T(3:end)' .* T_deseason(2:end-1).^(-2)));
     
     sigma1_n1 = sqrt( sum(Pr1T(3:end)' .* T_deseason(2:end-1).^(-2) .* (T_deseason(3:end) - ((1 - theta_base(1)) .* T_deseason(2:end-1) + theta_base(2)))) ...
-        / (sum(Pr1T(3:end)' .* T_deseason(2:end-1).^(-2))) );
-    
+        / (sum(Pr1T(3:end)' .* T_deseason(2:end-1).^(-2))) )
     
     mu2_n1 = (sum(Pr2T(3:end)' .* (T_deseason(3:end) - T_deseason(2:end-1)))) ...
         / (sum(Pr2T(3:end)));
@@ -40,29 +40,36 @@ elseif (model == 2)
 end
 
 % Update transition probabilities
-% First, compute forecast probabilities:
-Pr1_tminus1 = Pr1(1:end-1) * p(1, 1) + Pr2(1:end-1) * p(2, 1);
-Pr2_tminus1 = Pr1(1:end-1) * p(1, 2) + Pr2(1:end-1) * p(2, 2);
-% Second, compute the transitions:
-p(1, 1) =  sum(Pr1T(2:end) .* p(1, 1) .* ( Pr1(1:end-1) ./ Pr1_tminus1 )) ...
-    / sum(Pr1T(1:end-1));
-%p(1, 2) = 1 - p(1, 1);
-p(1, 2) =  sum(Pr2T(2:end) .* p(1, 2) .* ( Pr1(1:end-1) ./ Pr2_tminus1 )) ...
-    / sum(Pr1T(1:end-1));
+if (~useKim)
+    p1_n1 = (sum(Pr1T)) / (sum(Pr1T) + sum(Pr2T));
+    p2_n1 = (sum(Pr2T)) / (sum(Pr1T) + sum(Pr2T));
+    
+    fprintf("p_1: %.3f, p_2: %.3f.\n\n", p1_n1, p2_n1);
+else
+    % First, compute forecast probabilities:
+    Pr1_tminus1 = Pr1(1:end-1) * p(1, 1) + Pr2(1:end-1) * p(2, 1);
+    Pr2_tminus1 = Pr1(1:end-1) * p(1, 2) + Pr2(1:end-1) * p(2, 2);
+    % Second, compute the transitions:
+    p(1, 1) =  sum(Pr1T(2:end) .* p(1, 1) .* ( Pr1(1:end-1) ./ Pr1_tminus1 )) ...
+        / sum(Pr1T(1:end-1));
+    %p(1, 2) = 1 - p(1, 1);
+    p(1, 2) =  sum(Pr2T(2:end) .* p(1, 2) .* ( Pr1(1:end-1) ./ Pr2_tminus1 )) ...
+        / sum(Pr1T(1:end-1));
 
-p(2, 1) =  sum(Pr1T(2:end) .* p(2, 1) .* ( Pr2(1:end-1) ./ Pr1_tminus1 )) ...
-    / sum(Pr2T(1:end-1));
-%p(2, 2) = 1 - p(2, 1);
+    p(2, 1) =  sum(Pr1T(2:end) .* p(2, 1) .* ( Pr2(1:end-1) ./ Pr1_tminus1 )) ...
+        / sum(Pr2T(1:end-1));
+    %p(2, 2) = 1 - p(2, 1);
 
-p(2, 2) =  sum(Pr2T(2:end) .* p(2, 2) .* ( Pr2(1:end-1) ./ Pr2_tminus1)) ...
-    / sum(Pr2T(1:end-1));
+    p(2, 2) =  sum(Pr2T(2:end) .* p(2, 2) .* ( Pr2(1:end-1) ./ Pr2_tminus1)) ...
+        / sum(Pr2T(1:end-1));
 
-fprintf("p(1, 1): %.3f, p(1, 2): %.3f. p(2, 1): %.3f, p(2, 2): %.3f.\n\n",...
-    p(1, 1), p(1, 2), p(2, 1), p(2, 2));
-
-% Todo: Scrap
-p1_n1 = 0.98;
-p2_n1 = 0.95;
+    fprintf("p(1, 1): %.3f, p(1, 2): %.3f. p(2, 1): %.3f, p(2, 2): %.3f.\n\n",...
+        p(1, 1), p(1, 2), p(2, 1), p(2, 2));
+    
+    % Dummy values:
+    p1_n1 = 0.5;
+    p2_n1 = 0.5;
+end
 
 if (model == 1)
     Theta = [kappa_n1, sigma1_n1, p1_n1, mu2_n1, sigma2_n1, p2_n1];
