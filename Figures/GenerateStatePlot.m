b32b155c6568fe574c83805dcfc67995c6353f45
Fredrik <43887343+gbfredrik@users.noreply.github.com>
2022-04-25 21:51:19 +0200

    Add plotting functionalities

--- Figures/GenerateStatePlot.m
+++ Figures/GenerateStatePlot.m
@@ -19,7 +19,7 @@ subplot(2, 1, 1);
 plot(times, base, 'b.', times, shifted, 'rx')
 axis([1 times(end) min(T_d)-1 max(T_d)+1]);
 xlabel('Days');
-ylabel('Probability');
+ylabel('Deseasoned temperature');
 
 sub2 = subplot(2, 1, 2);
 hold on
@@ -27,24 +27,27 @@ bar(times, Pr2, 'b')
 plot(times, threshold .* ones(1, length(T_d)), 'r')
 axis([1 times(end) 0 1]);
 xlabel('Days');
-ylabel('Probability');
+ylabel('Probability of shifted state');
 hold off
 %sub2.Position = sub2.Position + [0 0 0 0.5];
+fprintf("Optima has %i base points and %i shifted points.\n", ...
+    sum(Pr1 >= 1-threshold), sum(Pr2 >= threshold));
 
 %%
-T_d_cum = cumsum(T_d);
-base_cum = (Pr1 >= 1-threshold) .* T_d_cum;
-shifted_cum = (Pr2 > threshold) .* T_d_cum;
-base_cum(base_cum == 0) = nan;
-shifted_cum(shifted_cum == 0) = nan;
-
-g = figure();
-plot(times, base_cum, 'b.', times, shifted_cum, 'rx')
+% T_d_cum = cumsum(T_d);
+% base_cum = (Pr1 >= 1-threshold) .* T_d_cum;
+% shifted_cum = (Pr2 > threshold) .* T_d_cum;
+% base_cum(base_cum == 0) = nan;
+% shifted_cum(shifted_cum == 0) = nan;
+% 
+% g = figure();
+% plot(times, base_cum, 'b.', times, shifted_cum, 'rx')
 
 %%
-sorted_delta = sort(T_d(2:end) - T_d(1:end-1));
-base_changes = (Pr1(2:end) >= 1-threshold) .* (sorted_delta);
-shifted_changes = (Pr2(2:end) >= threshold) .* (sorted_delta);
+tuple = [T_d(2:end) - T_d(1:end-1), Pr1(2:end)];
+tuple = sortrows(tuple);
+base_changes = (tuple(:,2) >= 1-threshold) .* (tuple(:,1));
+shifted_changes = (1 - tuple(:,2) >= threshold) .* (tuple(:,1));
 base_changes(base_changes == 0) = nan;
 shifted_changes(shifted_changes == 0) = nan;
 
@@ -53,7 +56,6 @@ hold on
 bar(times(2:end), base_changes)
 bar(times(2:end), shifted_changes)
 hold off
-
 %%
 pause(0.001)
 
--- Figures/IllustrateWienerProcess.m
+++ Figures/IllustrateWienerProcess.m
@@ -11,3 +11,25 @@ W = cumsum(dW);             % cumulative sum
 plot([0:dt:T],[0,W],'r-')   % plot W against t
 xlabel('t','FontSize',16)
 ylabel('W(t)','FontSize',16,'Rotation',0)
+
+%% 4 bull/bear
+clear
+rng(2)
+bull1 = normrnd(0.01, 0.125, 335, 1);
+bear1 = normrnd(0.02, 0.35, 30, 1);
+bull2 = normrnd(0.01, 0.125, 335, 1);
+bear2 = normrnd(0.02, 0.35, 30, 1);
+bull3 = normrnd(0.01, 0.125, 335, 1);
+bear3 = normrnd(0.02, 0.35, 30, 1);
+bull4 = normrnd(0.01, 0.125, 335, 1);
+bear4 = normrnd(0.02, 0.35, 30, 1);
+returns = [bull1; bear1; bull2; bear2; bull3; bear3; bull4; bear4];
+plot(returns)
+%% 2 bull/1 bear
+clear
+rng(3)
+bull1 = normrnd(1, 0.125, 100, 1);
+bear1 = normrnd(2, 0.45, 15, 1);
+bull2 = normrnd(1, 0.125, 65, 1);
+returns = [bull1; bear1; bull2];
+plot(returns)
